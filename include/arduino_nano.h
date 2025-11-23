ecb75d6379715661bde6c61561156beaf4f91987
TA Trikalinos <ttrikalin@mac.com>
2025-11-23 15:22:34 -0500

    reorganized files

--- include/arduino_nano.h
+++ include/arduino_nano.h
@@ -8,18 +8,18 @@
 /*****************************************************************************/
 
 // Thermocouple 
-#define THERMOCOUPLE_DATA        19
-#define THERMOCOUPLE_CLOCK       18
-#define THERMOCOUPLE_CHIP_SELECT  5
-#define ERROR_LED                32
+#define THERMOCOUPLE_DATA         11
+#define THERMOCOUPLE_CLOCK        13
+#define THERMOCOUPLE_CHIP_SELECT  10
+#define ERROR_LED                  8
 
-#define TEMPERATURE_SET_PIN      34  // ADC pin for temperature set potentiometer
-#define IRON_RELAY                2
+#define TEMPERATURE_SET_PIN       10  // ADC pin for temperature set potentiometer
+#define IRON_RELAY                 6
 
-#define SDA_PIN                  21
-#define SCL_PIN                  22
+#define SDA_PIN                   18
+#define SCL_PIN                   19
 
-//#define ZERO_CROSSING_PIN        35 
+//#define ZERO_CROSSING_PIN        XX 
 
 #define MCU_SERIAL_BAUD          115200
 #endif 
